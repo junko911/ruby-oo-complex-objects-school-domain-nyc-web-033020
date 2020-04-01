d713af5a571c976135ea4337d6f56c170efe4258
junko911 <junkotahara911@gmail.com>
2020-04-01 11:03:12 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
 # code here!
+require 'pry'
+
+class School
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+    
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def roster
+        @roster
+    end
+
+    def grade(num)
+        @roster[num]
+    end
+
+    def sort
+        sorted_roster = {}
+        @roster.each { |grade, names|
+            sorted_roster[grade] = names.sort
+        }
+        sorted_roster
+    end
+end
+
+school = School.new("Bayside High School")
+school.add_student("Zach Morris", 9)
+school.roster
+# binding.pry
